1a5a5a28723559fb65c5770e2fa1398398e55afa
havil007 <85561131+havil007@users.noreply.github.com>
2024-02-09 18:47:24 +0530

    Update streamlit_app.py
    Removed the Direct JSON result display

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,6 @@ streamlit.dataframe(fruits_to_show)
 # Display API Response
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
 # Normalize the JSOn format
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
