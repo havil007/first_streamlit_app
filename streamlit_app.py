24c21d77885f68547282e0aed7600383047cc7e3
havil007 <85561131+havil007@users.noreply.github.com>
2024-02-09 20:20:26 +0530

    Update streamlit_app.py
    Added exception handling for fruit selection logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,15 +25,20 @@ streamlit.dataframe(fruits_to_show)
 
 # Display API Response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# Normalize the JSOn format
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Display the normalized result as dataframe
-streamlit.dataframe(fruityvice_normalized)
-
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    streamlit.error("Please select fruit to get information")
+  else:
+    streamlit.write('The user entered ', fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # Normalize the JSOn format
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # Display the normalized result as dataframe
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+  
 # Add stop command
 streamlit.stop()
 
