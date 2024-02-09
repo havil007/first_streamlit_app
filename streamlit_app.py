6883c32224e41e8cfe61232dbdb3f7c979e920f6
havil007 <85561131+havil007@users.noreply.github.com>
2024-02-09 18:49:07 +0530

    Update streamlit_app.py
    Added selectbox to choose the fruit advice
    Modified the API result display to only show the selected values

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,9 @@ streamlit.dataframe(fruits_to_show)
 
 # Display API Response
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # Normalize the JSOn format
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
